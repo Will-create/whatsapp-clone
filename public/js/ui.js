a2d92c62ff1449325969cfe4aac6f4410e2dd458
Louis Bertson <louisbertson@gmail.com>
2021-04-12 21:13:16 +0000

    Improved chat UI

--- public/js/ui.js
+++ public/js/ui.js
@@ -82,3 +82,83 @@ COMPONENT('chat',  function(self) {
 
 	
 });
+COMPONENT('chatbody',  function(self) {
+
+	// var autoclosing;
+	// var system = false;
+	// var N = windo  w.Notification;
+
+	self.singleton();
+	self.readonly();
+	self.nocompile && self.nocompile();
+	self.template = Tangular.compile(`
+    <p data-id="{{id}}"  class="chat_message {{if incoming == true }} chat_receiver {{ fi}} ">
+	<span class="chat_messageAuthor">{{author}}</span>
+	{{body | raw}}
+	<span class="chat_messageBodyMetaTimeStamp">{{timestamp}}</span>
+	</p>
+	`);
+	self.items = {};
+	self.make = function() {
+		self.aclass('chat_body');
+		// self.event('click', '.fa-times-circle', function() {
+		// 	var el = $(this).closest('.ui-notification');
+		// 	self.close(+el.attr('data-id'));
+		// 	clearTimeout(autoclosing);
+		// 	autoclosing = null;
+		// 	self.autoclose();
+		// });
+		// self.event('click', 'a,button', function(e) {
+		// 	e.stopPropagation();
+		// });
+
+		self.event('click', '.ui-chat', function() {
+			var el = $(this);
+			var id = +el.attr('data-id');
+			// var obj = self.items[id];
+			// if (!obj || !obj.callback)
+			// 	return;
+			// obj.callback();
+			// self.open(id);
+		});
+
+		self.setter = function (value){
+			value.forEach(el => {
+				self.append(el.id,el.author,el.body,el.timestamp,el.incoming)
+			});
+		}
+
+		// if (config.native === true && N) {
+		// 	system = N.permission === 'granted';
+		// 	!system && N.requestPermission(function (permission) {
+		// 		system = permission === 'granted';
+		// 	});
+		// }
+	};
+
+	// self.close = function(id) {
+	// 	var obj = self.items[id];
+	// 	if (!obj)
+	// 		return;
+	// 	obj.callback = null;
+
+	// 	if (obj.system) {
+	// 		obj.system.onclick = null;
+	// 		obj.system.close();
+	// 		obj.system = null;
+	// 	}
+	// 	delete self.items[id];
+	// 	var item = self.find('div[data-id="{0}"]'.format(id));
+	// 	item.addClass('ui-notification-hide');
+	// 	setTimeout(function() {
+	// 		item.remove();
+	// 	}, 600);
+	// };
+	self.append = function(id, author, body, timestamp,incoming) {
+		var obj = { id: id , author: author, body:body,timestamp:timestamp,incoming : incoming};
+		self.items[obj.id] = obj;
+		self.element.append(self.template(obj));
+	};
+
+	
+});
--- tmp/js_ui.js
+++ tmp/js_ui.js
@@ -12,4 +12,10 @@ COMPONENT('chat',function(self){self.singleton();self.readonly();self.nocompile&
             </div>
 
         </div>
-    </div>`);self.items={};self.make=function(){self.aclass('sidebar_chats');self.event('click','.ui-chat',function(){var el=$(this);var id=+el.attr('data-id');});};self.append=function(id,img,message,name){var obj={id:id,img:img,message:message,name:name};self.items[obj.id]=obj;self.element.append(self.template(obj))}});
+    </div>`);self.items={};self.make=function(){self.aclass('sidebar_chats');self.event('click','.ui-chat',function(){var el=$(this);var id=+el.attr('data-id');});};self.append=function(id,img,message,name){var obj={id:id,img:img,message:message,name:name};self.items[obj.id]=obj;self.element.append(self.template(obj))}});COMPONENT('chatbody',function(self){self.singleton();self.readonly();self.nocompile&&self.nocompile();self.template=Tangular.compile(`
+    <p data-id="{{id}}"  class="chat_message {{if incoming == true}} chat_receiver {{ fi}} ">
+	<span class="chat_messageAuthor">{{author}}</span>
+	{{body | raw}}
+	<span class="chat_messageBodyMetaTimeStamp">{{timestamp}}</span>
+	</p>
+	`);self.items={};self.make=function(){self.aclass('chat_body');self.event('click','.ui-chat',function(){var el=$(this);var id=+el.attr('data-id');});self.setter=function(value){value.forEach(el=>{self.append(el.id,el.author,el.body,el.timestamp,el.incoming)})}};self.append=function(id,author,body,timestamp,incoming){var obj={id:id,author:author,body:body,timestamp:timestamp,incoming:incoming};self.items[obj.id]=obj;self.element.append(self.template(obj))}});
