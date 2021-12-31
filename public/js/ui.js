35f83e624c3a276d71b38ba4410025410ba20323
Louis Bertson <louisbertson@gmail.com>
2021-12-31 22:11:22 +0000

    updated j-chat.html

--- public/js/ui.js
+++ public/js/ui.js
@@ -7,25 +7,22 @@ COMPONENT('chat',  function(self) {
 	self.singleton();
 	self.readonly();
 	self.nocompile && self.nocompile();
-	self.template = Tangular.compile(`<div class="ui-chat" data-id="{{ id }}">
-       <div class="ui-chat-body">
-            <div class="ui-chat-bodyLeft">
-               <div class="ui-chat-image">
-                <img src="{{img}}"/>
-            	</div>
-            </div>
-            <div class="ui-chat-bodyRight">
-                <div class="ui-chat-name">{{ name }}</div>
-                <div class="ui-chat-message">{{ message | raw }}</div>
-            </div>
-
-        </div>
-    </div>`);
+	self.template = Tangular.compile(`<div data-id="{{ id }}" class="single_chat">
+	<div class="image_container">
+		<img src="{{ img }}">
+		</div>
+	<div class="content_container">
+		<div class="content_container_inner">
+			<p class="user_name"> <name>{{ username }}</name> <span> {{ last_update }}</span> </p>
+			<p class="chat_content">{{ last_message }}<span>{{ message_count }}</span> </p>
+   </div>
+	</div>
+</div>`);
 	
 	self.items = {};
 
 	self.make = function() {
-		self.aclass('sidebar_chats');
+		self.aclass('messages_section');
 		// self.event('click', '.fa-times-circle', function() {
 		// 	var el = $(this).closest('.ui-notification');
 		// 	self.close(+el.attr('data-id'));
@@ -38,9 +35,9 @@ COMPONENT('chat',  function(self) {
 		// 	e.stopPropagation();
 		// });
 
-		self.event('click', '.ui-chat', function() {
-			var el = $(this);
-			var id = +el.attr('data-id');
+		self.event('click', '.single_chat', function() {
+			// var el = $(this);
+			// var id = +el.attr('data-id');
 			// var obj = self.items[id];
 			// if (!obj || !obj.callback)
 			// 	return;
@@ -74,8 +71,8 @@ COMPONENT('chat',  function(self) {
 	// 		item.remove();
 	// 	}, 600);
 	// };
-	self.append = function(id, img, message, name) {
-		var obj = { id: id , img: img, message: message, name: name};
+	self.append = function(id, img, last_message, username, last_update, message_count) {
+		var obj = { id: id , img: img, last_message: last_message, username: username, last_update : last_update, message_count : message_count };
 		self.items[obj.id] = obj;
 		self.element.append(self.template(obj));
 	};
@@ -89,7 +86,7 @@ COMPONENT('chatbody',  function(self) {
 	self.singleton();
 	self.readonly();
 	self.nocompile && self.nocompile();
-	self.template = Tangular.compile(`<p data-id="{{id}}"  class="chat_message {{if incoming == true }} chat_receiver {{ fi}} ">
+	self.template = Tangular.compile(`<p data-id="{{ id }}"  class="chat_message {{if incoming == true }} chat_receiver {{ fi}} ">
 	<span class="chat_messageAuthor">{{author}}</span>
 	{{body | raw}}
 	<span class="chat_messageBodyMetaTimeStamp">{{timestamp}}</span>
