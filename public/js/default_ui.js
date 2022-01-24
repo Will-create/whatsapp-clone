e5afb474b6a17d14ae24282b47a44178897f4584
Louis Bertson <louisbertson@gmail.com>
2022-01-24 14:58:06 +0000

    Added auto focus to search bar

--- public/js/default_ui.js
+++ public/js/default_ui.js
@@ -18,18 +18,7 @@
 				}
 			  });
 		});
-		 $('.back_trigger').click(function(){
-			$('.search_bar').val('');
-			$('.search_bar').keyup();
-			$('.search_bar').animate({opacity : 0},200);
 
-			$('.back_trigger').css('display','none');
-			$('.back_button').css('display','none');
-			$('.search_bar').css('display','none');
-			$('.back_trigger').addClass('hidden');
-			
-	
-		});
 		$('.back_button').click(function(){
 			$('.ion-ios-telephone-outline').addClass('hidden');
 			$('.ion-ios-videocam-outline').addClass('hidden');
@@ -65,10 +54,10 @@
 		});
 		$('#search').click(function(){	
 			$('.search_bar').removeClass('hidden');
-			$('.search_bar').focus();
 			$('.search_bar').animate({opacity : 1 , width : '100%'},200);
 			$('.back_trigger').removeClass('hidden');
 			$('.back_trigger').css('display','block');
+			$('.search_bar').focus();
 		});
 		if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
 			REDIRECT('/');
