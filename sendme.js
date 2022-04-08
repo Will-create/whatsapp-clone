30ee872d14d30fa2c8c64e83dc4692db62c616e5
Louis Bertson <louisbertson@gmail.com>
2022-04-08 01:08:50 +0000

    removed bad multiple columns in chat

--- sendme.js
+++ sendme.js
@@ -6,15 +6,15 @@
 const options = {};
 
 // options.ip = '127.0.0.1';
- options.port = 8004;
+ options.port = 8000;
 // options.unixsocket = require('path').join(require('os').tmpdir(), 'app_name');
 // options.unixsocket777 = true;
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 // options.inspector = 9229;
 // options.watch = ['private'];
- options.livereload = true;
-// options.https = { key: Fs.readFileSync('keys/agent2-key.pem'), cert: Fs.readFileSync('keys/agent2-cert.pem')};
+ //options.livereload = true;
+ //options.https = { key: Fs.readFileSync('keys/agent2-key.pem'), cert: Fs.readFileSync('keys/agent2-cert.pem')};
 // options.watcher = true; // enables watcher for the release mode only controlled by the app `F.restart()`
 // options.edit = 'wss://www.yourcodeinstance.com/?id=projectname'
 
